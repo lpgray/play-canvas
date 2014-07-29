5aabd6103cd336254e55f25d962e196ebe952530
lpgray <rayzy1991@gmail.com>
2014-07-29 13:01:07 +0800

    build bet game

---
+++ bet-game/build/placeholder.js
@@ -0,0 +1,24 @@
+/**
+ * Placeholders
+ */
+function ajax(url, callback) {
+    $.ajax({
+        url: url,
+        success: callback,
+        dataType: 'html',
+        cache : false
+    });
+}
+function checkLogon(callback){
+	return ajax('json/loginCheck.json', callback);
+};
+function getUserInfo(custNum, callback){
+	return ajax('json/getUserInfo.json', callback);
+};
+function getAllOddInfo(callback){
+	return ajax('json/getBetInfo.json', callback);
+};
+function submitWagerInfo(data, callback){
+    console.info('执行投注', data);
+	return ajax('json/saveBet.json', callback);
+};
